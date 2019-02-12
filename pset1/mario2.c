eb02b1f7558cddff6fb9409d21f114a8fd5c9a46
ryaneh <ryaneh1@gmail.com>
2019-02-12 14:18:26 -0800

    Mario.c More Comfortable
    Mario.c More Comfortable

---
+++ pset1/mario2.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    
+    //do whlie loop. does 1 action, then repeats until while is met
+    do{
+        height = get_int("Height: ");
+    
+        //Height < 1 program asks if height is not a number or is less than 1
+        //a <= 8 so it's max height of 8
+    }while(height <1 || height >= 9);
+    
+    //create a loop to RUN 'height' amount of times. Ex if height is 1 run once
+    for(i=0; i<=height; i++;){
+        f
+               
+        
+    }
+    
+    printf("%d \n",height);    
+}
